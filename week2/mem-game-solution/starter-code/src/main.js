344d4ba57e347000338d3aab46bd3d3f294fcf5d
nickborbe <nickborbe@gmail.com>
2019-06-19 17:51:27 -0400

    memory game partial solution added

---
+++ week2/mem-game-solution/starter-code/src/main.js
@@ -0,0 +1,70 @@
+var cards = [
+  { name: 'aquaman',         img: 'aquaman.jpg' },
+  { name: 'batman',          img: 'batman.jpg' },
+  { name: 'captain america', img: 'captain-america.jpg' },
+  { name: 'fantastic four',  img: 'fantastic-four.jpg' },
+  { name: 'flash',           img: 'flash.jpg' },
+  { name: 'green arrow',     img: 'green-arrow.jpg' },
+  { name: 'green lantern',   img: 'green-lantern.jpg' },
+  { name: 'ironman',         img: 'ironman.jpg' },
+  { name: 'spiderman',       img: 'spiderman.jpg' },
+  { name: 'superman',        img: 'superman.jpg' },
+  { name: 'the avengers',    img: 'the-avengers.jpg' },
+  { name: 'thor',            img: 'thor.jpg' },
+  { name: 'aquaman',         img: 'aquaman.jpg' },
+  { name: 'batman',          img: 'batman.jpg' },
+  { name: 'captain america', img: 'captain-america.jpg' },
+  { name: 'fantastic four',  img: 'fantastic-four.jpg' },
+  { name: 'flash',           img: 'flash.jpg' },
+  { name: 'green arrow',     img: 'green-arrow.jpg' },
+  { name: 'green lantern',   img: 'green-lantern.jpg' },
+  { name: 'ironman',         img: 'ironman.jpg' },
+  { name: 'spiderman',       img: 'spiderman.jpg' },
+  { name: 'superman',        img: 'superman.jpg' },
+  { name: 'the avengers',    img: 'the-avengers.jpg' },
+  { name: 'thor',            img: 'thor.jpg' }
+];
+var memoryGame = new MemoryGame(cards);
+
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+  var html = '';
+  memoryGame.cards.forEach(function (pic) {
+    html += '<div class="card" data-card-name="'+ pic.name +'">';
+    html += '  <div class="back" name="'+ pic.img +'"></div>';
+    html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
+    html += '</div>';
+  });
+
+  // Add all the div's to the HTML
+  document.querySelector('#memory_board').innerHTML = html;
+
+  // Bind the click event of each element to a function
+ 
+
+  $('.back').click(function(){
+
+    $(this).removeClass('back').addClass('front')
+    $(this).siblings('.front').addClass('back').removeClass('front');
+
+
+    memoryGame.currentPair.push($(this).parent())
+
+    if(memoryGame.currentPair.length ===2){
+
+      // memoryGame.checkIfPair()
+
+      // console.log('there are 2 things in the current hand, time to compare', memoryGame.currentPair[0].data('card-name'), memoryGame.currentPair[1].data('card-name'))
+    }
+    
+
+    
+  })
+
+
+
+
+
+});
+
+
---
+++ week2/mem-game-solution/starter-code/src/memory.js
@@ -0,0 +1,9 @@
+class MemoryGame {
+  constructor(card){
+    this.cards = cards;
+    this.currentPair = [];
+  }
+  shuffleCards() {}
+  checkIfPair(card1, card2) {}
+  isFinished() {}
+}
